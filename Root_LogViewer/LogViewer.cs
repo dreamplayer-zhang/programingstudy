71f4120618cdc45fc4aa5f99305e02ce3c84099d
mgkim <forget018@naver.com>
2020-04-29 10:55:45 +0900

    Root_LogViewer

---
+++ Root_LogViewer/LogViewer.cs
@@ -0,0 +1,24 @@
+﻿using RootTools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Root_LogViewer
+{
+    public class LogViewer : NotifyProperty
+    {
+        string _sInfo = ""; 
+        public string p_sInfo
+        {
+            get { return _sInfo; }
+            set
+            {
+                if (_sInfo == value) return;
+                _sInfo = value;
+                OnPropertyChanged(); 
+            }
+        }
+    }
+}
---
+++ Root_LogViewer/LogViewer_UI.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows.Controls;
+
+namespace Root_LogViewer
+{
+    /// <summary>
+    /// LogViewer_UI.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class LogViewer_UI : UserControl
+    {
+        public LogViewer_UI()
+        {
+            InitializeComponent();
+        }
+
+        LogViewer m_logViewer; 
+        public void Init(LogViewer logViewer)
+        {
+            m_logViewer = logViewer;
+            DataContext = logViewer; 
+        }
+
+        private void buttonOpen_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Log Files (*.csv)|*.csv";
+            dlg.InitialDirectory = "c:\\Log"; 
+            if (dlg.ShowDialog() == false)
+            {
+                m_logViewer.p_sInfo = "Log File Open Cancel !!"; 
+                return;
+            }
+            string sTitle = dlg.SafeFileName;
+            string sFile = dlg.FileName;
+            string sPath = sFile.Substring(0, sFile.Length - sTitle.Length - 1);
+            DirectoryInfo dir = new DirectoryInfo(sPath);
+            if (dir.Exists == false)
+            {
+                m_logViewer.p_sInfo = "Can't Find Log Folder : " + sPath;
+                return;
+            }
+            tabLogGroup.Items.Clear(); 
+            //m_logViewer.OpenLogFiles(sPath);
+            if (m_logViewer.p_sInfo != "OK") return; 
+
+        }
+    }
+}
