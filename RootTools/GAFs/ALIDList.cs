87813b69de563521a26635c81d46b3b0c9139845
Daniel <usjung@ati2000.co.kr>
2020-07-20 11:30:44 +0900

    ALID Patch

--- RootTools/GAFs/ALIDList.cs
+++ RootTools/GAFs/ALIDList.cs
@@ -11,18 +11,14 @@ namespace RootTools.GAFs
     public class ALIDList : NotifyProperty
     {
         #region List ALID
-        ObservableCollection<ALID> _aALID = new ObservableCollection<ALID>();
-        public ObservableCollection<ALID> p_aALID
-        {
-            get { return _aALID; }
-        }
+        public List<ALID> m_aALID = new List<ALID>();
 
         public void SaveFile(string sFile)
         {
             try
             {
                 StreamWriter sw = new StreamWriter(new FileStream(sFile, FileMode.Create));
-                foreach (ALID alid in p_aALID)
+                foreach (ALID alid in m_aALID)
                 {
                     alid.Save(sw);
                 }
@@ -33,13 +29,12 @@ namespace RootTools.GAFs
 
         public void ClearALID()
         {
-            foreach (ALID alid in p_aALID)
+            foreach (ALID alid in m_aALID)
             {
                 alid.p_sMsg = "";
                 alid.p_bSet = false;
             }
             p_alarmBlink = false;
-            p_aSetALID.Clear();
         }
         #endregion
 
@@ -56,15 +51,15 @@ namespace RootTools.GAFs
             }
         }
 
-        Brush _brushAlarm = Brushes.White; 
+        Brush _brushAlarm = Brushes.White;
         public Brush p_brushAlarm
-        { 
+        {
             get { return _brushAlarm; }
             set
             {
                 if (_brushAlarm == value) return;
                 _brushAlarm = value;
-                OnPropertyChanged(); 
+                OnPropertyChanged();
             }
         }
         private bool _alarmBlink = false;
@@ -83,7 +78,7 @@ namespace RootTools.GAFs
         }
         public void ShowPopup()
         {
-            if (ALIDList_PopupUI.m_bShow) return; 
+            if (ALIDList_PopupUI.m_bShow) return;
             ALIDList_PopupUI alidPopup = new ALIDList_PopupUI();
             alidPopup.Init(this);
             alidPopup.Show();
@@ -91,34 +86,27 @@ namespace RootTools.GAFs
         #endregion 
 
         #region List Set ALID
-        ObservableCollection<ALID> _aSetALID = new ObservableCollection<ALID>();
-        public ObservableCollection<ALID> p_aSetALID
-        {
-            get { return _aSetALID; }
-        }
+        public ObservableCollection<ALID> p_aSetALID { get; set; }
 
         DispatcherTimer m_timerSetALID = new DispatcherTimer();
-        Queue<ALID> m_qSetALID = new Queue<ALID>();
         public void SetALID(ALID alid)
         {
-            m_qSetALID.Enqueue(alid);
             p_sInfo = alid.p_sModule + " : " + alid.p_sDesc + ", " + alid.p_sMsg;
             p_brushAlarm = Brushes.Red;
             p_alarmBlink = true;
 
-            
+
         }
 
         private void M_timerSetALID_Tick(object sender, EventArgs e)
         {
             for (int n = p_aSetALID.Count - 1; n >= 0; n--)
             {
-                if (p_aSetALID[n].p_bSet == false) p_aSetALID.RemoveAt(n); 
+                if (p_aSetALID[n].p_bSet == false) p_aSetALID.RemoveAt(n);
             }
-            while (m_qSetALID.Count > 0)
+            foreach (ALID alid in m_aALID)
             {
-                ALID alid = m_qSetALID.Dequeue(); 
-                if (IsExistinSetALID(alid) == false)
+                if (alid.p_bSet && (IsExistSetALID(alid) == false))
                 {
                     ShowPopup();
                     p_aSetALID.Add(alid);
@@ -127,14 +115,14 @@ namespace RootTools.GAFs
             p_brushAlarm = (p_aSetALID.Count > 0) ? Brushes.Red : Brushes.White;
         }
 
-        bool IsExistinSetALID(ALID setALID)
-		{
-            foreach (ALID alid in p_aSetALID)
-			{
-                if (alid.p_id == setALID.p_id) return true; 
-			}
-            return false; 
-		}
+        bool IsExistSetALID(ALID alid)
+        {
+            foreach (ALID al in p_aSetALID)
+            {
+                if (al.p_id == alid.p_id) return true;
+            }
+            return false;
+        }
         #endregion
 
         #region Tree
@@ -168,6 +156,7 @@ namespace RootTools.GAFs
         public TreeRoot m_treeRoot;
         public void Init(string id, GAF gaf)
         {
+            p_aSetALID = new ObservableCollection<ALID>();
             m_id = id;
             m_log = gaf.m_log;
             m_aGroup = gaf.m_aGroup;
