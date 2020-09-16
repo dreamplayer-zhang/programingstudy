96e99019a61055fcab60aef73c192c9e7f9dc31d
김민규 <forget018@naver.com>
2020-09-16 17:23:48 +0900

    ..

--- Root_ASIS/Module/ASIS.cs
+++ Root_ASIS/Module/ASIS.cs
@@ -1,7 +1,9 @@
 ﻿using RootTools;
 using RootTools.Control;
 using RootTools.Module;
+using RootTools.Trees;
 using System;
+using System.Threading;
 
 namespace Root_ASIS.Module
 {
@@ -25,7 +27,7 @@ namespace Root_ASIS.Module
         public enum eBuzzer
         {
             Error,
-            Call,
+            Finish,
             Warning,
             Home
         }
@@ -46,5 +48,75 @@ namespace Root_ASIS.Module
             base.ThreadStop();
         }
 
+        #region ModuleRun
+        protected override void InitModuleRuns()
+        {
+            AddModuleRunList(new Run_Delay(this), false, "Just Time Delay");
+            AddModuleRunList(new Run_Buzzer(this), false, "Run Buzzer");
+        }
+
+        public class Run_Delay : ModuleRunBase
+        {
+            ASIS m_module;
+            public Run_Delay(ASIS module)
+            {
+                m_module = module;
+                InitModuleRun(module);
+            }
+
+            double m_secDelay = 2;
+            public override ModuleRunBase Clone()
+            {
+                Run_Delay run = new Run_Delay(m_module);
+                run.m_secDelay = m_secDelay;
+                return run;
+            }
+
+            public override void RunTree(Tree tree, bool bVisible, bool bRecipe = false)
+            {
+                m_secDelay = tree.Set(m_secDelay, m_secDelay, "Delay", "Time Delay (sec)", bVisible);
+            }
+
+            public override string Run()
+            {
+                Thread.Sleep((int)(1000 * m_secDelay));
+                return "OK";
+            }
+        }
+
+        public class Run_Buzzer : ModuleRunBase
+        {
+            ASIS m_module;
+            public Run_Buzzer(ASIS module)
+            {
+                m_module = module;
+                InitModuleRun(module);
+            }
+
+            eBuzzer m_eBuzzer = eBuzzer.Home; 
+            double m_secBuzzer = 2;
+            public override ModuleRunBase Clone()
+            {
+                Run_Buzzer run = new Run_Buzzer(m_module);
+                run.m_eBuzzer = m_eBuzzer; 
+                run.m_secBuzzer = m_secBuzzer;
+                return run;
+            }
+
+            public override void RunTree(Tree tree, bool bVisible, bool bRecipe = false)
+            {
+                m_eBuzzer = (eBuzzer)tree.Set(m_eBuzzer, m_eBuzzer, "Buzzer", "Select Buzzer", bVisible); 
+                m_secBuzzer = tree.Set(m_secBuzzer, m_secBuzzer, "Time", "Buzzer Run Time (sec)", bVisible);
+            }
+
+            public override string Run()
+            {
+                m_module.m_doBuzzer.Write(m_eBuzzer, true);
+                Thread.Sleep((int)(1000 * m_secBuzzer));
+                m_module.m_doBuzzer.Write(m_eBuzzer, false);
+                return "OK";
+            }
+        }
+        #endregion
     }
 }
