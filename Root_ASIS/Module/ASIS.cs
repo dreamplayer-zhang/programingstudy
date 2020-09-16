2d1ac233e0c4096f3e792b3fa33ec9c95757d1bd
김민규 <forget018@naver.com>
2020-09-16 14:02:07 +0900

    Root_ASIS

---
+++ Root_ASIS/Module/ASIS.cs
@@ -0,0 +1,50 @@
+﻿using RootTools;
+using RootTools.Control;
+using RootTools.Module;
+using System;
+
+namespace Root_ASIS.Module
+{
+    public class ASIS : ModuleBase
+    {
+        #region ToolBox
+        DIO_Os m_doBuzzer; 
+
+        public override void GetTools(bool bInit)
+        {
+            p_sInfo = m_toolBox.Get(ref m_doBuzzer, this, "Buzzer", Enum.GetNames(typeof(eBuzzer))); 
+            if (bInit) InitTools();
+        }
+
+        void InitTools()
+        {
+        }
+        #endregion
+
+        #region Buzzer
+        public enum eBuzzer
+        {
+            Error,
+            Call,
+            Warning,
+            Home
+        }
+
+        public void RunBuzzer(eBuzzer buzzer, bool bOn)
+        {
+            m_doBuzzer.Write(buzzer, bOn); 
+        }
+        #endregion
+
+        public ASIS(string id, IEngineer engineer)
+        {
+            base.InitBase(id, engineer);
+        }
+
+        public override void ThreadStop()
+        {
+            base.ThreadStop();
+        }
+
+    }
+}
---
+++ Root_ASIS/Module/LoadEV.cs
@@ -0,0 +1,44 @@
+﻿using RootTools.Control;
+using RootTools.Module;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Root_ASIS.Module
+{
+    public class LoadEV : ModuleBase
+    {
+        #region ToolBox
+        DIO_I2O2 m_dioEV;
+        DIO_I m_diTop;
+        DIO_I m_diCheck;
+        DIO_I m_diIonizer;
+        DIO_I m_diPaper;
+        DIO_I m_diPaperCheck;
+        DIO_I m_diPaperFull; 
+        DIO_O m_doIonBlow;
+        DIO_O m_doAlignBlow;
+
+        public override void GetTools(bool bInit)
+        {
+            p_sInfo = m_toolBox.Get(ref m_dioEV, this, "Elevator", "Up", "Down");
+            p_sInfo = m_toolBox.Get(ref m_diTop, this, "Top");
+            p_sInfo = m_toolBox.Get(ref m_diCheck, this, "Check");
+            p_sInfo = m_toolBox.Get(ref m_diIonizer, this, "Ionizer");
+            p_sInfo = m_toolBox.Get(ref m_diPaper, this, "Paper");
+            p_sInfo = m_toolBox.Get(ref m_diPaperCheck, this, "PaperCheck");
+            p_sInfo = m_toolBox.Get(ref m_diPaperFull, this, "PaperFull");
+            p_sInfo = m_toolBox.Get(ref m_doIonBlow, this, "IonBlow");
+            p_sInfo = m_toolBox.Get(ref m_doAlignBlow, this, "AlignBlow");
+            if (bInit) InitTools();
+        }
+
+        void InitTools()
+        {
+        }
+        #endregion
+
+    }
+}
