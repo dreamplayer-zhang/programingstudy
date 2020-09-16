43ea47b410c63a88b25a0c364db1c2d913aab988
태훈 황 <thhwang@ati2000.co.kr>
2020-09-16 14:39:01 +0900

    Template Matching 추가 및 CLR_IP, IP 코드 수정

--- RootTools_Cpp/IP.h
+++ RootTools_Cpp/IP.h
@@ -12,12 +12,27 @@ public:
 	static void Threshold(BYTE* pSrc, int nW, int nH, int nThrehold, bool bDark, BYTE* pDst);
 	static void Labeling(BYTE* pSrc, BYTE* pBin, int nW, int nH, bool bDark, BYTE* pDst, std::vector<LabeledData>& vtLabeled);
 
-	//Vision
+	// Vision
+	/*
+	※ 참고
+	  - 함수 파라미터 순서 (Src - Input Image, Dst - Output Image, **Out**** - Result Data, ...)
+	  - 오버로딩 되어있는 함수 사용 (Output이 영상인 경우 제외)
+		1. CS에서 원본 이미지를 새로운 이미지 버퍼에 카피하여 함수의 입력으로 사용
+		2. 원본 이미지와 ROI 정보를 입력으로 받아 연산 진행 (Unsafe 사용)
+	  - 
+	*/
 	static void Threshold(BYTE* pSrc, BYTE* pDst, int nW, int nH, bool bDark, int threshold);
-	
+	static void Threshold(BYTE* pSrc, BYTE* pDst, int nMemW, int nMemH, Point ptLeftTop, Point ptRightBot, bool bDark, int threshold);
+
 	static float Average(BYTE* pSrc, int nW, int nH);	
-	static void Labeling(BYTE* pSrc, BYTE* pBin, std::vector<LabeledData>& vtLabeled, int nW, int nH, bool bDark);
+	static float Average(BYTE* pSrc, int nMemW, int nMemH, Point ptLeftTop, Point ptRightBot);
 	
+	static void Labeling(BYTE* pSrc, BYTE* pBin, std::vector<LabeledData>& vtOutLabeled, int nW, int nH, bool bDark);
+	static void Labeling(BYTE* pSrc, BYTE* pBin, std::vector<LabeledData>& vtOutLabeled, int nMemW, int nMemH, Point ptLeftTop, Point ptRightBot, bool bDark);
+
+	// Method 참고 : https://docs.opencv.org/2.4/doc/tutorials/imgproc/histograms/template_matching/template_matching.html
+	static int TemplateMatching(BYTE* pSrc, BYTE* pTemp, Point& outMatchPoint, int nSrcW, int nSrcH, int nTempW, int nTempH, int method = 5);
+
 	// Filtering
 	static void GaussianBlur(BYTE* pSrc, BYTE* pDst, int nW, int nH, int nSigma);
 	static void MedianBlur(BYTE* pSrc, BYTE* pDst, int nW, int nH, int nFilterSz);
