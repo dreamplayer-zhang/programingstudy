b929dfc2c14936cd3714dadd4e9e2a9afe841d27
JTL <jaydenlee@ati2000.co.kr>
2020-10-19 09:32:13 +0900

    초안 Mainwindow 작업중

---
+++ Root_CAMELLIA/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+using System.Windows.Media;
+
+namespace Root_CAMELLIA
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (this.WindowState == WindowState.Maximized)
+            {
+                MaximizeButton.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                NormalizeButton.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+        }
+        #region Title Bar
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Maximized;
+            NormalizeButton.Visibility = Visibility.Visible;
+            MaximizeButton.Visibility = Visibility.Collapsed;
+        }
+        private void NormalizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Normal;
+            MaximizeButton.Visibility = Visibility.Visible;
+            NormalizeButton.Visibility = Visibility.Collapsed;
+        }
+        private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                if (this.WindowState == WindowState.Maximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    MaximizeButton.Visibility = Visibility.Visible;
+                    NormalizeButton.Visibility = Visibility.Collapsed;
+                }
+                else
+                {
+                    this.WindowState = WindowState.Maximized;
+                    NormalizeButton.Visibility = Visibility.Visible;
+                    MaximizeButton.Visibility = Visibility.Collapsed;
+                }
+            }
+            else
+            {
+                this.DragMove();
+            }
+        }
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+    }
+}
