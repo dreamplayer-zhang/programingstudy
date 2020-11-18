6c1e63edc2cdbf6989cc72752f9c88a3fe2e64b4
Joseph <joseph@ati2000.co.kr>
2020-11-18 17:42:28 +0900

    SaveBMP 수정

--- RootTools/Image/ImageData.cs
+++ RootTools/Image/ImageData.cs
@@ -507,13 +507,8 @@ namespace RootTools
 		}
 		unsafe void FileSaveBMP(string sFile, IntPtr ptr, CRect rect)
 		{
-			int width = (int)(rect.Right * 0.25);
-			if (width * 4 != rect.Right) rect.Right = (width + 1) * 4;
-
-			//if (rect.Width % 4 != 0)
-			//{
-			//	rect.Right += 4 - rect.Width % 4;
-			//}
+			//int width = (int)(rect.Right * 0.25);
+			//if (width * 4 != rect.Right) rect.Right = (width + 1) * 4;
 
 			FileStream fs = new FileStream(sFile, FileMode.Create, FileAccess.Write);
 			BinaryWriter bw = new BinaryWriter(fs);
@@ -559,6 +554,10 @@ namespace RootTools
 					bw.Write(Convert.ToByte(i));
 					bw.Write(Convert.ToByte(255));
 				}
+			}
+			if (rect.Width % 4 != 0)
+			{
+				rect.Right += 4 - rect.Width % 4;
 			}
 			byte[] aBuf = new byte[p_nByte * rect.Width];
 			for (int i = rect.Height - 1; i >= 0; i--)
