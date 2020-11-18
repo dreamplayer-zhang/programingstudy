13cadc5feead873f4b98fcd94f20103356a15518
jwkim <jwkim@ati2000.co.kr>
2020-11-18 17:27:24 +0900

    Handler추가.

--- Root_CAMELLIA/MainWindow.xaml.cs
+++ Root_CAMELLIA/MainWindow.xaml.cs
@@ -154,9 +154,6 @@ namespace Root_CAMELLIA
             }
             //recipe.Visibility = Visibility.Hidden;
             //recipe.Close();
-
-
-
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
---
+++ Root_CAMELLIA/Module/Stage.cs
@@ -0,0 +1,79 @@
+﻿using RootTools;
+using RootTools.Control;
+using RootTools.Light;
+using RootTools.Memory;
+using RootTools.Module;
+using RootTools.Trees;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Root_CAMELLIA.Module
+{
+    public class Stage : ModuleBase
+    {
+        #region ToolBox
+        Axis m_axisRotate;
+        AxisXY m_axisXZ;
+        DIO_O m_doVac;
+        DIO_O m_doBlow;
+        MemoryPool m_memoryPool;
+        readonly MemoryGroup m_memoryGroup;
+        LightSet m_lightSet;
+
+        public override void GetTools(bool bInit)
+        {
+            p_sInfo = m_toolBox.Get(ref m_axisRotate, this, "Rotate");
+            p_sInfo = m_toolBox.Get(ref m_axisXZ, this, "Camera XZ");
+            p_sInfo = m_toolBox.Get(ref m_doVac, this, "Stage Vacuum");
+            p_sInfo = m_toolBox.Get(ref m_doBlow, this, "Stage Blow");
+            p_sInfo = m_toolBox.Get(ref m_memoryPool, this, "Memory");
+            p_sInfo = m_toolBox.Get(ref m_lightSet, this);
+        }
+        #endregion
+
+        public Stage(string id, IEngineer engineer)
+        {   
+            base.InitBase(id, engineer);
+            //            InitMemory();
+        }
+
+        public override void ThreadStop()
+        {
+            base.ThreadStop();
+        }
+
+
+        protected override void InitModuleRuns()
+        {
+            AddModuleRunList(new Run_Delay(this), false, "Time Delay");
+        }
+
+        public class Run_Delay : ModuleRunBase
+        {
+            Stage m_module;
+            public Run_Delay(Stage module)
+            {
+                m_module = module;
+                InitModuleRun(module);
+            }
+
+            public override ModuleRunBase Clone()
+            {
+                Run_Delay run = new Run_Delay(m_module);
+                return run;
+            }
+
+            public override void RunTree(Tree tree, bool bVisible, bool bRecipe = false)
+            {
+            }
+
+            public override string Run()
+            {
+                return "OK";
+            }
+        }
+    }
+}
