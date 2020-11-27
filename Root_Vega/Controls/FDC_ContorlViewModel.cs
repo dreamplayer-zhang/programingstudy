f3fca3e8076ed50e3757e29aefd49588a7836252
usjung <usjung@ati2000.co.kr>
2020-11-27 10:02:14 +0900

    201127 usjung

--- Root_Vega/Controls/FDC_ContorlViewModel.cs
+++ Root_Vega/Controls/FDC_ContorlViewModel.cs
@@ -83,7 +83,7 @@ namespace Root_Vega
         {
             m_DialogService = DialogService;
 
-			m_timer.Interval = TimeSpan.FromMilliseconds(0.01);
+			m_timer.Interval = TimeSpan.FromMilliseconds(10);
 			m_timer.Tick += new EventHandler(timer_Tick);
 			m_timer.Start();
 		}
@@ -143,20 +143,8 @@ namespace Root_Vega
             }
         }
 
-		public void SetFDC(int nslave, double dValue)
-		{
-			for (int i = 0; i < m_CVM.Count; i++)
-			{
-				if (nslave == m_SlaveList[i])
-				{
-					m_CVM[i].p_CurrentValue = dValue;
-				}
-			}
-		}
 		public Root_Vega.Module.FDC m_FDC;
         Registry m_reg;
-
-
 
 		public void Loaded()
 		{ 
@@ -187,7 +175,10 @@ namespace Root_Vega
 			int CVM_Counter = m_FDC.p_lData;
 			for (int index = 0; index < CVM_Counter; index++)
 			{
-				//m_CVM[index].p_CurrentValue = m_FDC.m_aData[index].p_fValue;
+				if (m_CVM != null)
+				{
+					m_CVM[index].p_CurrentValue = m_FDC.m_aData[index].p_fValue;
+				}
 			}
 		}
 
