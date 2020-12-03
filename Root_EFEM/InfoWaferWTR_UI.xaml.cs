496a61d5aba6607ecbbfa4c9ec367d5f8e1e5618
김민규 <forget018@naver.com>
2020-12-03 15:41:08 +0900

    InfoWaferUI

--- Root_EFEM/InfoWaferWTR_UI.xaml.cs
+++ Root_EFEM/InfoWaferWTR_UI.xaml.cs
@@ -1,32 +1,49 @@
 ﻿using Root_EFEM.Module;
+using RootTools;
 using RootTools.Trees;
+using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace Root_EFEM
 {
     /// <summary>
     /// InfoWaferWTR_UI.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class InfoWaferWTR_UI : UserControl
+    public partial class InfoWaferWTR_UI : UserControl, ITool
     {
         public InfoWaferWTR_UI()
         {
             InitializeComponent();
         }
 
-        IWTR m_wtr; 
-        public void Init(IWTR wtr)
+        IEngineer m_engineer; 
+        List<WTRArm> m_aArm; 
+        public void Init(string id, List<WTRArm> aArm, IEngineer engineer)
         {
-            m_wtr = wtr;
-            DataContext = wtr; 
+            p_id = id; 
+            m_aArm = aArm;
+            m_engineer = engineer; 
+            InitComboBox(); 
+            InitTree();
+            InitTimer(); 
         }
+
+        #region ITool
+        public string p_id { get; set; }
+
+        public UserControl p_ui { get { return this; } }
+
+        public void ThreadStop() { }
+        #endregion
 
         #region Tree
         public TreeRoot m_treeRoot;
         void InitTree()
         {
-            m_treeRoot = new TreeRoot(m_wtr.p_id, null);
+            m_treeRoot = new TreeRoot(p_id, null);
             m_treeRoot.UpdateTree += M_treeRoot_UpdateTree;
             treeRootUI.Init(m_treeRoot);
             RunTree(Tree.eMode.Init);
@@ -40,29 +57,51 @@ namespace Root_EFEM
         public void RunTree(Tree.eMode eMode)
         {
             m_treeRoot.p_eMode = eMode;
-            //if (m_wtr.p_infoWafer != null) m_wtr.p_infoWafer.RunTree(m_treeRoot.GetTree(m_wtr.p_infoWafer.p_id));
+            foreach (WTRArm arm in m_aArm)
+            {
+                if (arm.p_infoWafer != null) arm.p_infoWafer.RunTree(m_treeRoot.GetTree(arm.p_infoWafer.p_id));
+            }
         }
         #endregion
 
-        #region Button Click
-        private void buttonUpperAramAdd_Click(object sender, RoutedEventArgs e)
-        {
+        #region Timer
+        DispatcherTimer m_timer = new DispatcherTimer();
 
+        void InitTimer()
+        {
+            m_timer.Interval = TimeSpan.FromSeconds(1);
+            m_timer.Tick += M_timer_Tick;
+            m_timer.Start();
         }
 
-        private void buttonUpperArmRemove_Click(object sender, RoutedEventArgs e)
+        private void M_timer_Tick(object sender, EventArgs e)
         {
+            if (m_treeRoot.m_bFocus) return;
+            RunTree(Tree.eMode.Init);
+        }
+        #endregion
 
+        #region UI control
+        List<string> m_asArm = new List<string>(); 
+        void InitComboBox()
+        {
+            foreach (WTRArm arm in m_aArm) m_asArm.Add(arm.m_id);
+            comboArm.ItemsSource = m_asArm;
+            comboArm.SelectedIndex = 0; 
         }
 
-        private void buttonLowerAramAdd_Click(object sender, RoutedEventArgs e)
+        private void buttonAdd_Click(object sender, RoutedEventArgs e)
         {
-
+            int iArm = comboArm.SelectedIndex;
+            if ((iArm < 0) || (iArm >= m_aArm.Count)) return;
+            m_aArm[iArm].p_infoWafer = new InfoWafer(m_aArm[iArm].m_id + ".Recover", m_engineer); 
         }
 
-        private void buttonLowerArmRemove_Click(object sender, RoutedEventArgs e)
+        private void buttonRemove_Click(object sender, RoutedEventArgs e)
         {
-
+            int iArm = comboArm.SelectedIndex;
+            if ((iArm < 0) || (iArm >= m_aArm.Count)) return;
+            m_aArm[iArm].p_infoWafer = null; 
         }
         #endregion
     }
