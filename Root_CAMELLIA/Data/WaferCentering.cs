fb2c9390d28a71903ad5511e013fece9b03a51c9
HJYOO <hjyoo@ati2000.co.kr>
2020-12-04 10:30:06 +0900

    Converter 추가 진행중 Centering 기능 추가

--- Root_CAMELLIA/Data/WaferCentering.cs
+++ Root_CAMELLIA/Data/WaferCentering.cs
@@ -29,7 +29,7 @@ namespace Root_CAMELLIA.Data
         public Point m_ptLT = new Point();
         public Point m_ptRT = new Point();
         public Point m_ptRB = new Point();
-
+        public RPoint m_ptCenter = new RPoint();
         //public void FindEdge(byte[] imgBuf, ePos pos)
         //{
         //    Mat matSrc = new Mat("emgutest.bmp");
@@ -37,10 +37,65 @@ namespace Root_CAMELLIA.Data
 
         //}
         //public void FindEdge(byte[] ImgBuf, ePos pos, CPoint ptROI)
+        public void CalCenterPoint(CPoint ptROI, double resolutionX, double resolutionY, RPoint ptLTPulse, RPoint ptRTPulse, RPoint ptRBPulse)
+        {
+            double LTX = ptLTPulse.X - (((ptROI.X / 2) + m_ptLT.X) * resolutionX * 10);
+            double LTY = ptLTPulse.Y + (((ptROI.Y / 2) - m_ptLT.Y) * resolutionY * 10);
+
+            double RTX = ptRTPulse.X - (((ptROI.X / 2) + m_ptRT.X) * resolutionX * 10);
+            double RTY = ptRTPulse.Y + (((ptROI.Y / 2) - m_ptRT.Y) * resolutionY * 10);
+
+            double RBX = ptRBPulse.X - (((ptROI.X / 2) + m_ptRB.X) * resolutionX * 10);
+            double RBY = ptRBPulse.Y + (((ptROI.Y / 2) - m_ptRB.Y) * resolutionY * 10);
+
+            PointF ptLT = new PointF((float)LTX, (float)LTY);
+            PointF ptRT = new PointF((float)RTX, (float)RTY);
+            PointF ptRB = new PointF((float)RBX, (float)RBY);
+
+            Get3PointCircle(ptLT, ptRT, ptRB, out double cx, out double cy, out double r);
+
+            m_ptCenter.X = cx;
+            m_ptCenter.Y = cy;
+        }
+
+        void Get3PointCircle(PointF p0, PointF p1, PointF p2, out double cx, out double cy, out double r)
+        {
+            double dy1, dy2, d, d2, yi;
+            PointF P1 = new PointF((p0.X + p1.X) / 2, (p0.Y + p1.Y) / 2);
+            PointF P2 = new PointF((p0.X + p2.X) / 2, (p0.Y + p2.Y) / 2);
+            dy1 = p0.Y - p1.Y;
+            dy2 = p0.Y - p2.Y;
+            cx = 0;
+            cy = 0;
+            r = 0;
+            if (dy1 != 0)
+            {
+                d = (p1.X - p0.X) / dy1;
+                yi = P1.Y - d * P1.X;
+                if (dy2 != 0)
+                {
+                    d2 = (p2.X - p0.X) / dy2;
+                    if (d != d2) cx = (yi - (P2.Y - d2 * P2.X)) / (d2 - d);
+                    else return;
+                }
+                else if (p2.X - p0.X == 0) return;
+                else cx = P2.X;
+            }
+            else if (dy2 != 0 && p1.X - p0.X != 0)
+            {
+                d = (p2.X - p0.X) / dy2;
+                yi = P2.Y - d * P2.X;
+                cx = P1.X;
+            }
+            else return;
+            cy = d * cx + yi;
+            r = Math.Sqrt((p0.X - cx) * (p0.X - cx) + (p0.Y - cy) * (p0.Y - cy));
+        }
+
         public void FindEdge(ImageData ImgData, CPoint ptROI, int nSearchRange, int nSearchLength, int nSearchLevel, eDir dir)
         {
 
-            Bitmap bitmap = new Bitmap(@"C:\Users\cgkim\Desktop\ttt\tttt.bmp"); //This is your bitmap
+            //            Bitmap bitmap = new Bitmap(@"C:\Users\cgkim\Desktop\ttt\tttt.bmp"); //This is your bitmap
 
 
             //byte[] result = null;
@@ -50,23 +105,56 @@ namespace Root_CAMELLIA.Data
             //    bitmap.Save(stream, bitmap.RawFormat);
             //    result = stream.ToArray();
             //} 
-            
 
-            Mat matSrc = new Mat(new Size(ptROI.X, ptROI.Y), DepthType.Cv8U, 3, ImgData.GetPtr(), (int)ImgData.p_Stride);
+            Mat matSrc = new Mat();
+             Mat matTest = new Mat(new Size(ptROI.X, ptROI.Y), DepthType.Cv8U, 3, ImgData.GetPtr(), (int)ImgData.p_Stride);
+            Image<Gray, byte> imageGray = new Image<Gray, byte>(ptROI.X, ptROI.Y, (int)ImgData.p_Stride, ImgData.GetPtr());
             Mat matInsp = new Mat(ptROI.X, ptROI.Y, DepthType.Cv8U, 3);
             //Image<Gray, byte> imageGray = new Image<Gray, byte>(bitmap);
 
             //byte[] pImg = matSrc.GetRawData();
+            matSrc = imageGray.Mat;
 
             matSrc.CopyTo(matInsp);
-            CvInvoke.MedianBlur(matInsp, matInsp, 5);
+            CvInvoke.MedianBlur(matInsp, matInsp, 7);
 
+
+            //Emgu.CV.UI.ImageViewer.Show(matSrc);
+            //Emgu.CV.UI.ImageViewer.Show(matInsp);
+            Point vector = new Point(0, 1);
+            Point edge = new Point();
+
+            switch (dir)
+            {
+                case eDir.LT:
+                    vector = new Point(0, 1);
+                    edge = GetEdgePoint(matInsp, new PointF(ptROI.X / 2, 0), vector, nSearchRange, ptROI.Y, nSearchLevel);
+                    m_ptLT = edge;
+                    break;
+                case eDir.RT:
+                    vector = new Point(0, 1);
+                    edge = GetEdgePoint(matInsp, new PointF(ptROI.X / 2, 0), vector, nSearchRange, ptROI.Y, nSearchLevel);
+                    m_ptRT = edge;
+                    break;
+                case eDir.RB:
+                    vector = new Point(0, -1);
+                    edge = GetEdgePoint(matInsp, new PointF(ptROI.X / 2, ptROI.Y), vector, nSearchRange, ptROI.Y, nSearchLevel);
+                    m_ptRB = edge;
+                    break;
+            }
+
+
+
+
+
+            CvInvoke.Circle(matTest, edge, 10, new MCvScalar(0,255,0));
+            Emgu.CV.UI.ImageViewer.Show(matTest);
             //for(int i = 0; i < nSearchLength; i++)
             //{
             //    switch (dir)
             //    {
             //        case eDir.LT:
- 
+
             //            break;
             //        case eDir.RT:
             //            break;
@@ -76,13 +164,13 @@ namespace Root_CAMELLIA.Data
             //}
 
 
-           // Emgu.CV.UI.ImageViewer.Show(matInsp, "ttt");
+            // Emgu.CV.UI.ImageViewer.Show(matInsp, "ttt");
 
             //Image<Gray, byte> imageGray = new Image<Gray, byte>(,);
 
 
 
-            
+
 
 
             //Image<Gray, byte> imageGray = new Image<Gray, byte>(ptROI.X, ptROI.Y);
@@ -154,11 +242,11 @@ namespace Root_CAMELLIA.Data
         {
             Point ptEdge = new Point();
             double prox = GetProx(Image, ptStart, vector, nSearchRange, nSearchLength, nSearchLevel);
-            if(prox < 10)
+            if (prox < 10)
             {
                 return ptEdge;
             }
-          //  ptEdge =  GetEdgePoint(Image, ptStart, vector, )
+            ptEdge = GetEdgePoint(Image, ptStart, vector, prox, nSearchRange, nSearchLength);
 
             return ptEdge;
         }
@@ -177,7 +265,7 @@ namespace Root_CAMELLIA.Data
 
             int x = 0;
             int y = 0;
-            for(int i = 0; i < nSearchRange; i++)
+            for (int i = 0; i < nSearchRange; i++)
             {
                 x = (int)(point.X + positionX + 0.5);
                 y = (int)(point.Y + positionY + 0.5);
@@ -254,12 +342,13 @@ namespace Root_CAMELLIA.Data
             double vectorX = 0;
             double vectorY = 0;
 
-            byte LineSum = 0;
+
             for (int i = 0; i < nSearchLength; i++)
             {
-                for(int j = 0; j < nSearchRange; j++)
+                int LineSum = 0;
+                for (int j = 0; j < nSearchRange; j++)
                 {
-                    int x = (int)(point.X + vectorX + 0.5);
+                    int x = (int)(point.X + j + 0.5);
                     int y = (int)(point.Y + vectorY + 0.5);
                     CheckPointRange(Image, ref x, ref y);
                     LineSum += pImg[y * Image.Width + x];
@@ -269,7 +358,7 @@ namespace Root_CAMELLIA.Data
                 {
                     min = LineAvg;
                 }
-                if(LineAvg > max)
+                if (LineAvg > max)
                 {
                     max = LineAvg;
                 }
