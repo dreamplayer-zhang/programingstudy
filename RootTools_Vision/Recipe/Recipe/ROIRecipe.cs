b72ce22225444d23e43ee8a83bcd9de683ff3793
Hohun-Hohun <66814350+Hohun-Hohun@users.noreply.github.com>
2020-12-17 10:55:54 +0900

    ROI, Mask 추가.

---
+++ RootTools_Vision/Recipe/Recipe/ROIRecipe.cs
@@ -0,0 +1,94 @@
+﻿using RootTools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace RootTools_Vision
+{
+    public class ROIRecipe : RecipeBase
+    {
+        CPoint originPoint;
+        CRect boundingBox;
+        List<RecipeType_Mask> maskList;
+        
+        public List<RecipeType_Mask> MaskList 
+        { 
+            get => maskList; 
+            set => maskList = value; 
+        }
+        public CPoint OriginPoint 
+        { 
+            get => originPoint; 
+            set => originPoint = value; 
+        }
+
+        [XmlIgnore]
+        public CRect BoudingBox 
+        { 
+            get => boundingBox; 
+            set => boundingBox = value; 
+        }
+
+        public ROIRecipe()
+        {
+            this.originPoint = new CPoint();
+            this.boundingBox = new CRect();
+            this.maskList = new List<RecipeType_Mask>();
+            
+        }
+
+        public ROIRecipe(CPoint _originPoint)
+        {
+            this.originPoint = _originPoint;
+            this.boundingBox = new CRect();
+            this.maskList = new List<RecipeType_Mask>();
+        }
+
+        public void AddMask(RecipeType_Mask _mask)
+        {
+            this.maskList.Add(_mask);
+
+            CalcBoundingBox();
+        }
+
+        public void SetOriginPoint(int x, int y)
+        {
+            this.OriginPoint.X = x;
+            this.OriginPoint.Y = y;
+        }
+
+        public void CalcBoundingBox()
+        {
+            int minX = int.MaxValue;
+            int minY = int.MaxValue;
+            int maxX = int.MinValue;
+            int maxY = int.MinValue;
+
+            foreach (RecipeType_Mask mask in maskList)
+            {
+                minX = minX > mask.BoundingBox.Left ? mask.BoundingBox.Left : minX;
+                minY = minY > mask.BoundingBox.Top ? mask.BoundingBox.Top : minY;
+                maxX = maxX < mask.BoundingBox.Right ? mask.BoundingBox.Right : maxX;
+                maxY = maxY < mask.BoundingBox.Bottom ? mask.BoundingBox.Bottom : maxY;
+            }
+
+            boundingBox.Left = minX;
+            boundingBox.Top = minY;
+            boundingBox.Right = maxX;
+            boundingBox.Bottom = maxY;
+        }
+
+        public override bool Read(string recipePath)
+        {
+            return true;
+        }
+
+        public override bool Save(string recipePath)
+        {
+            return true;
+        }
+    }
+}
