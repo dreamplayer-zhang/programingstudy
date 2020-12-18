713e67a08f08bd37342f1675f39d565e8996f27d
이지혜 <jhlee@ati2000.co.kr>
2020-12-18 16:48:00 +0900

    snap 붙일준비

--- Root_AOP01_Inspection/UI/1. SETUP/2. RecipeWizard/ViewModel/Recipe45D_ViewModel.cs
+++ Root_AOP01_Inspection/UI/1. SETUP/2. RecipeWizard/ViewModel/Recipe45D_ViewModel.cs
@@ -1,10 +1,11 @@
 ﻿using System.Windows.Input;
-
+using Root_AOP01_Inspection.Module;
 namespace Root_AOP01_Inspection
 {
     class Recipe45D_ViewModel : ObservableObject
     {
         Setup_ViewModel m_Setup;
+
         public Recipe45D_ViewModel(Setup_ViewModel setup)
         {
             m_Setup = setup;
@@ -17,6 +18,14 @@ namespace Root_AOP01_Inspection
                 return new RelayCommand(() =>
                 {
                     m_Setup.Set_RecipeWizardPanel();
+                });
+            }
+        }
+        public ICommand btnSnap
+        {
+            get
+            {
+                return new RelayCommand(() => {
                 });
             }
         }
