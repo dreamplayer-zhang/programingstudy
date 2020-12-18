74e1cf109db3efba4c35fbf4105af7ee85d6c8dc
younjongLee <71359510+younjongLee@users.noreply.github.com>
2020-12-18 10:19:35 +0900

    AOP inspection Clean mod

--- Root_AOP01_Inspection/Engineer/AOP01_Handler.cs
+++ Root_AOP01_Inspection/Engineer/AOP01_Handler.cs
@@ -87,7 +87,7 @@ namespace Root_AOP01_Inspection
             switch (m_eWTR)
             {
                 case eWTR.Cymechs: m_wtr = new WTR_Cymechs("WTR", m_engineer); break;
-                default: m_wtr = new WTR("WTR", m_engineer); break;
+                default: m_wtr = new WTRCleanUnit("WTR&CleanUnit", m_engineer); break;
             }
             InitModule(m_wtr);
         }
