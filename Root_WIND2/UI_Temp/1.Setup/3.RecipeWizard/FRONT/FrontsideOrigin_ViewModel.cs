7712123cec71e9a03548865d343fa75932dc606d
Hohun-Hohun <66814350+Hohun-Hohun@users.noreply.github.com>
2021-01-27 08:46:20 +0900

    UI 개선작업.

--- Root_WIND2/UI_Temp/1.Setup/3.RecipeWizard/FRONT/FrontsideOrigin_ViewModel.cs
+++ Root_WIND2/UI_Temp/1.Setup/3.RecipeWizard/FRONT/FrontsideOrigin_ViewModel.cs
@@ -1,12 +1,184 @@
-﻿using System;
+﻿using RootTools;
+using RootTools_Vision;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Root_WIND2.UI_Temp
 {
     class FrontsideOrigin_ViewModel : ObservableObject
     {
+        private readonly FrontsideOrigin_ImageViewer_ViewModel imageViewerVM;
+        public FrontsideOrigin_ImageViewer_ViewModel ImageViewerVM
+        {
+            get => this.imageViewerVM;
+        }
+
+
+        public FrontsideOrigin_ViewModel()
+        {
+            this.imageViewerVM = new FrontsideOrigin_ImageViewer_ViewModel();
+            this.imageViewerVM.init(GlobalObjects.Instance.GetNamed<ImageData>("FrontImage"), GlobalObjects.Instance.Get<DialogService>());
+        }
+
+
+        #region [Properties]
+
+        private string displayViewerState = FRONT_ORIGIN_VIEWER_STATE.Normal.ToString();
+        public string DisplayViewerState
+        {
+            get => this.displayViewerState;
+            set
+            {
+                SetProperty<string>(ref this.displayViewerState, value);
+            }
+        }
+
+        private bool isOriginChecked = false;
+        public bool IsOriginChecked
+        {
+            get => this.isOriginChecked;
+            set
+            {
+                if (value == true)
+                {
+                    this.IsPitchChecked = false;
+                    this.IsRularChecked = false;
+                }
+                SetProperty<bool>(ref this.isOriginChecked, value);
+            }
+        }
+
+        private bool isPitchChecked = false;
+        public bool IsPitchChecked
+        {
+            get => this.isPitchChecked;
+            set
+            {
+                if (value == true)
+                {
+                    this.IsOriginChecked = false;
+                    this.IsRularChecked = false;
+                }
+                SetProperty<bool>(ref this.isPitchChecked, value);
+            }
+        }
+
+        private bool isRularChecked = false;
+        public bool IsRularChecked
+        {
+            get => this.isRularChecked;
+            set
+            {
+                if (value == true)
+                {
+                    this.IsPitchChecked = false;
+                    this.IsOriginChecked = false;
+                }
+                SetProperty<bool>(ref this.isRularChecked, value);
+            }
+        }
+
+        #endregion
+
+        #region [Command]
+        public RelayCommand btnOriginCommand
+        {
+            get 
+            {
+                return new RelayCommand(() =>
+                {
+                    if(this.IsOriginChecked == true)
+                    {
+                        this.ImageViewerVM.ViewerState = FRONT_ORIGIN_VIEWER_STATE.Origin;
+                        this.DisplayViewerState = this.ImageViewerVM.ViewerState.ToString();
+                    }
+                    else
+                    {
+                        this.ImageViewerVM.ViewerState = FRONT_ORIGIN_VIEWER_STATE.Normal;
+                        this.DisplayViewerState = this.ImageViewerVM.ViewerState.ToString();
+
+                    }
+                });
+            }
+        }
+
+        public RelayCommand btnPitchCommand
+        {
+            get
+            {
+                return new RelayCommand(() =>
+                {
+                    if (this.IsPitchChecked == true)
+                    {
+                        this.ImageViewerVM.ViewerState = FRONT_ORIGIN_VIEWER_STATE.Pitch;
+                        this.DisplayViewerState = this.ImageViewerVM.ViewerState.ToString();
+                    }
+                    else
+                    {
+                        this.ImageViewerVM.ViewerState = FRONT_ORIGIN_VIEWER_STATE.Normal;
+                        this.DisplayViewerState = this.ImageViewerVM.ViewerState.ToString();
+                    }
+                });
+            }
+        }
+
+        public RelayCommand btnRularCommand
+        {
+            get
+            {
+                return new RelayCommand(() =>
+                {
+                    if (this.IsRularChecked == true)
+                    {
+                        this.ImageViewerVM.ViewerState = FRONT_ORIGIN_VIEWER_STATE.Rular;
+                        this.DisplayViewerState = this.ImageViewerVM.ViewerState.ToString();
+                    }
+                    else
+                    {
+                        this.ImageViewerVM.ViewerState = FRONT_ORIGIN_VIEWER_STATE.Normal;
+                        this.DisplayViewerState = this.ImageViewerVM.ViewerState.ToString();
+                    }
+                });
+            }
+        }
+
+        public RelayCommand btnOpenCommand
+        {
+            get
+            {
+                return new RelayCommand(() =>
+                {
+                    this.ImageViewerVM._openImage();
+                });
+            }
+        }
+
+        public RelayCommand btnSaveCommand
+        {
+            get
+            {
+                return new RelayCommand(() =>
+                {
+                    this.ImageViewerVM._saveImage();
+                });
+            }
+        }
+
+        public RelayCommand btnClearCommand
+        {
+            get
+            {
+                return new RelayCommand(() =>
+                {
+                    this.ImageViewerVM._clearImage();
+                });
+            }
+        }
+
+        #endregion
     }
 }
