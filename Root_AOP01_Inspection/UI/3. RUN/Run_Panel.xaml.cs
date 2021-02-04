672b402a6ab8ff73018f6c68d52b577774f4af29
이윤종 <yjlee94@ati2000.co.kr>
2021-02-04 09:16:25 +0900

    YJ

--- Root_AOP01_Inspection/UI/3. RUN/Run_Panel.xaml.cs
+++ Root_AOP01_Inspection/UI/3. RUN/Run_Panel.xaml.cs
@@ -51,7 +51,7 @@ namespace Root_AOP01_Inspection
             LoadportA_State.DataContext = loadport1;
             LoadportB_State.DataContext = loadport2;
             RTR_State.DataContext = wtrcleanunit;
-            Machine_State.DataContext = EQ.p_eState;
+            Machine_State.DataContext = EQ.m_EQ;
             InitFFU();
             InitTimer();
         }
