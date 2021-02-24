a85746ceac530ef2e646fc1506e2891e45f1a888
Jay <kyjang@ati2000.co.kr>
2021-02-24 10:36:37 +0900

    test

---
+++ Root_CAMELLIA/UI_Dialog/Dlg_OHT.xaml.cs
@@ -0,0 +1,32 @@
+﻿using RootTools.OHTNew;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Root_CAMELLIA.UI_Dialog
+{
+    /// <summary>
+    /// Dlg_OHT.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Dlg_OHT : Window
+    {
+        public Dlg_OHT()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
--- Root_CAMELLIA/UI_UserControl/HomeProgress_UI.xaml.cs
+++ Root_CAMELLIA/UI_UserControl/HomeProgress_UI.xaml.cs
@@ -94,7 +94,7 @@ namespace Root_CAMELLIA.UI_UserControl
             if (bShow) this.Show();
             if (m_handler.m_wtr.p_eState == ModuleBase.eState.Home)
             {
-                progressWTR.Value = (int)(100 * Math.Min((m_swWTR.ElapsedMilliseconds / (((WTR_RND)m_handler.m_wtr).m_secHome * 1000)), 1.0));
+                progressWTR.Value = (int)(100 * Math.Min((double)((double)m_swWTR.ElapsedMilliseconds / (((WTR_RND)m_handler.m_wtr).m_secHome * 1000)), (double)1.0));
             }
             else if (m_handler.m_wtr.p_eState == ModuleBase.eState.Ready) progressWTR.Value = 100;
             else if (m_handler.m_wtr.p_eState == ModuleBase.eState.Error) progressWTR.Value = 0;    //working
@@ -102,7 +102,7 @@ namespace Root_CAMELLIA.UI_UserControl
 
             if (m_handler.m_loadport[0].p_eState == ModuleBase.eState.Home)
             {
-                progressLP1.Value = (int)(100 * Math.Min((m_swLoadport[0].ElapsedMilliseconds / (m_handler.m_aLoadport[0].p_secHome * 1000)), 1.0));
+                progressLP1.Value = (int)(100 * Math.Min((double)((double)m_swLoadport[0].ElapsedMilliseconds / (m_handler.m_aLoadport[0].p_secHome * 1000)), (double)1.0));
             }
             else if (m_handler.m_loadport[0].p_eState == ModuleBase.eState.Ready) progressLP1.Value = 100;
             else if (m_handler.m_loadport[0].p_eState == ModuleBase.eState.Error) progressLP1.Value = 0;    //working
@@ -110,7 +110,7 @@ namespace Root_CAMELLIA.UI_UserControl
 
             if (m_handler.m_loadport[1].p_eState == ModuleBase.eState.Home)
             {
-                progressLP2.Value = (int)(100 * Math.Min((m_swLoadport[1].ElapsedMilliseconds / (m_handler.m_aLoadport[1].p_secHome * 1000)), 1.0));
+                progressLP2.Value = (int)(100 * Math.Min((double)((double)m_swLoadport[1].ElapsedMilliseconds / (m_handler.m_aLoadport[1].p_secHome * 1000)), (double)1.0));
             }
             else if (m_handler.m_loadport[1].p_eState == ModuleBase.eState.Ready) progressLP2.Value = 100;
             else if (m_handler.m_loadport[1].p_eState == ModuleBase.eState.Error) progressLP2.Value = 0;    //working
@@ -118,7 +118,7 @@ namespace Root_CAMELLIA.UI_UserControl
 
             if (m_handler.m_Aligner.p_eState == ModuleBase.eState.Home)
             {
-                progressAL.Value = (int)(100 * Math.Min((m_swAligner.ElapsedMilliseconds / (10 * 1000)), 1.0));
+                progressAL.Value = (int)(100 * Math.Min((double)((double)m_swAligner.ElapsedMilliseconds / (20 * 1000)), (double)1.0));
             }
             else if (m_handler.m_Aligner.p_eState == ModuleBase.eState.Ready) progressAL.Value = 100;
             else if (m_handler.m_Aligner.p_eState == ModuleBase.eState.Error) progressAL.Value = 0; //working
@@ -126,7 +126,7 @@ namespace Root_CAMELLIA.UI_UserControl
 
             if (m_handler.m_camellia.p_eState == ModuleBase.eState.Home)
             {
-                progressVS.Value = (int)(100 * Math.Min((m_swVision.ElapsedMilliseconds / (20 * 1000)), 1.0));
+                progressVS.Value = (int)(100 * Math.Min((double)((double)m_swVision.ElapsedMilliseconds / (30 * 1000)), (double)1.0));
             }
             else if (m_handler.m_camellia.p_eState == ModuleBase.eState.Ready) progressVS.Value = 100;
             else if (m_handler.m_camellia.p_eState == ModuleBase.eState.Error) progressVS.Value = 0;    //working
