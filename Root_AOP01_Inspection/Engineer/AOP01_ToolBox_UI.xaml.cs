d69a159db6dc83e272aa3dafd06e5637ef6e00d5
조은성 <escho@ati2000.co.kr>
2021-02-26 15:28:23 +0900

    AOP01 Maint Toolbox 이동

--- Root_AOP01_Inspection/Engineer/AOP01_ToolBox_UI.xaml.cs
+++ Root_AOP01_Inspection/Engineer/AOP01_ToolBox_UI.xaml.cs
@@ -5,11 +5,11 @@ using System.Windows.Controls;
 namespace Root_AOP01_Inspection
 {
     /// <summary>
-    /// AUP01_Engineer_UI.xaml에 대한 상호 작용 논리
+    /// AOP01_ToolBox_UI.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class AOP01_Engineer_UI : UserControl
+    public partial class AOP01_ToolBox_UI : UserControl
     {
-        public AOP01_Engineer_UI()
+        public AOP01_ToolBox_UI()
         {
             InitializeComponent();
         }
@@ -18,7 +18,10 @@ namespace Root_AOP01_Inspection
         public void Init(AOP01_Engineer engineer)
         {
             m_engineer = engineer;
-            handlerUI.Init(engineer.m_handler); // 2020-11-13 
+            logViewUI.Init(LogView.m_logView);
+            treeRootUI.Init(engineer.m_treeRoot);
+            loginUI.Init(engineer.m_login);
+            toolBoxUI.Init(engineer.ClassToolBox());
             engineer.RunTree(Tree.eMode.Init);
         }
     }
