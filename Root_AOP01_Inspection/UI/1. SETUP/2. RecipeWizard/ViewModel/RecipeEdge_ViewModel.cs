bc3af8bec0f1c12a4e1a57667c131a6421dbddf0
Joseph <joseph@ati2000.co.kr>
2021-03-03 17:27:16 +0900

    사이드쪽 저장기능 갱신

--- Root_AOP01_Inspection/UI/1. SETUP/2. RecipeWizard/ViewModel/RecipeEdge_ViewModel.cs
+++ Root_AOP01_Inspection/UI/1. SETUP/2. RecipeWizard/ViewModel/RecipeEdge_ViewModel.cs
@@ -129,49 +129,28 @@ namespace Root_AOP01_Inspection
 
 		private void saveCurrentEdge()
 		{
-			//string[] keywords = new string[] { "Main", "SideLeft", "SideTop", "SideBot", "SideRight" };
-			//현재 ViewModel에 있는 edgebox를 저장한다.
 			MainVision mainVision = ((AOP01_Handler)m_Engineer.ClassHandler()).m_mainVision;
 
 			//bot
 			if (m_ImageViewerBot_VM.TRectList.Count == 6)
 			{
-				//tempList = new List<TRect>(viewer.TRectList);
-				MainVision.Run_SurfaceInspection surfaceInspection = (MainVision.Run_SurfaceInspection)mainVision.CloneModuleRun(App.SideBotModuleName);
-				surfaceInspection.EdgeList = new List<TRect>(m_ImageViewerBot_VM.TRectList).ToArray();
-				//surfaceInspection.UpdateTree();
-				//surfaceInspection.RefreshTree();
+				mainVision.SetRectInfo(m_ImageViewerBot_VM.TRectList, App.SideBotModuleName);
 			}
 			//top
 			if (m_ImageViewerTop_VM.TRectList.Count == 6)
 			{
-				//tempList = new List<TRect>(viewer.TRectList);
-				MainVision.Run_SurfaceInspection surfaceInspection = (MainVision.Run_SurfaceInspection)mainVision.CloneModuleRun(App.SideTopModuleName);
-				surfaceInspection.EdgeList = new List<TRect>(m_ImageViewerTop_VM.TRectList).ToArray();
-				//surfaceInspection.UpdateTree();
-				//surfaceInspection.RefreshTree();
+				mainVision.SetRectInfo(m_ImageViewerTop_VM.TRectList, App.SideTopModuleName);
 			}
 			//left
 			if (m_ImageViewerLeft_VM.TRectList.Count == 6)
 			{
-				//tempList = new List<TRect>(viewer.TRectList);
-				MainVision.Run_SurfaceInspection surfaceInspection = (MainVision.Run_SurfaceInspection)mainVision.CloneModuleRun(App.SideLeftModuleName);
-				surfaceInspection.EdgeList = new List<TRect>(m_ImageViewerLeft_VM.TRectList).ToArray();
-				//surfaceInspection.UpdateTree();
-				//surfaceInspection.RefreshTree();
+				mainVision.SetRectInfo(m_ImageViewerLeft_VM.TRectList, App.SideLeftModuleName);
 			}
 			//right
-			if (m_ImageViewerBot_VM.TRectList.Count == 6)
+			if (m_ImageViewerRight_VM.TRectList.Count == 6)
 			{
-				//tempList = new List<TRect>(viewer.TRectList);
-				MainVision.Run_SurfaceInspection surfaceInspection = (MainVision.Run_SurfaceInspection)mainVision.CloneModuleRun(App.SideBotModuleName);
-				surfaceInspection.EdgeList = new List<TRect>(m_ImageViewerBot_VM.TRectList).ToArray();
-				//surfaceInspection.UpdateTree();
-				//surfaceInspection.RefreshTree();
+				mainVision.SetRectInfo(m_ImageViewerRight_VM.TRectList, App.SideRightModuleName);
 			}
-
-
-			//surfaceInspection.UpdateTree();
 		}
 		public ICommand btnBack
 		{
