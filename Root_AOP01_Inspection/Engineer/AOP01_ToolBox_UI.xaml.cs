60953e5a28d9645ce5a290ba6aeca343f4ba4b35
조은성 <escho@ati2000.co.kr>
2021-03-05 08:32:31 +0900

    ...

--- Root_AOP01_Inspection/Engineer/AOP01_ToolBox_UI.xaml.cs
+++ Root_AOP01_Inspection/Engineer/AOP01_ToolBox_UI.xaml.cs
@@ -18,7 +18,7 @@ namespace Root_AOP01_Inspection
         public void Init(AOP01_Engineer engineer)
         {
             m_engineer = engineer;
-            logViewUI.Init(LogView.m_logView);
+            logViewUI.Init(LogView._logView);
             treeRootUI.Init(engineer.m_treeRoot);
             loginUI.Init(engineer.m_login);
             toolBoxUI.Init(engineer.ClassToolBox());
