b35652b0df70367e1bacc0174d8977e822c1d529
이지혜 <67890421+jihyel22@users.noreply.github.com>
2021-04-08 14:16:24 +0900

    stacking 동작까지 확인
    stacking 동작까지 확인

--- Root_VEGA_P_Vision/Module/Vision/Run_ZStack.cs
+++ Root_VEGA_P_Vision/Module/Vision/Run_ZStack.cs
@@ -69,47 +69,51 @@ namespace Root_VEGA_P_Vision.Module
             if (p_sZStackGrabMode == null) return "Grab Mode : ZStack Grab == Null";
             try
             {
-                //AxisXY axisXY = m_module.m_stage.m_axisXY;
-                //Axis axisZ = mainOpt.m_axisZ;
+                ZStackGrabMode.SetLight(true);
 
-                //ZStackGrabMode.SetLight(true);
+                AxisXY axisXY = m_module.m_stage.m_axisXY;
+                Axis axisZ = mainOpt.m_axisZ;
 
-                //if (m_module.Run(axisXY.StartMove(new RPoint(ZStackGrabMode.m_rpAxisCenter.X, ZStackGrabMode.m_rpAxisCenter.Y))))
-                //    return p_sInfo;
-                //if (m_module.Run(axisXY.WaitReady()))
-                //    return p_sInfo;
 
-                ////InfoPod.ePod parts = m_module.p_infoPod.p_ePod;
-                ////Vision.eUpDown upDown = (Vision.eUpDown)Enum.ToObject(typeof(Vision.eUpDown), m_module.p_infoPod.p_bTurn);
-                ////MemoryData mem = mainOpt.GetMemoryData(parts, Vision.MainOptic.eInsp.Stack, upDown);
+                if (m_module.Run(axisXY.StartMove(new RPoint(ZStackGrabMode.m_rpAxisCenter.X, ZStackGrabMode.m_rpAxisCenter.Y))))
+                    return p_sInfo;
+                if (m_module.Run(axisXY.WaitReady()))
+                    return p_sInfo;
+
+                //InfoPod.ePod parts = m_module.p_infoPod.p_ePod;
+                //Vision.eUpDown upDown = (Vision.eUpDown)Enum.ToObject(typeof(Vision.eUpDown), m_module.p_infoPod.p_bTurn);
+                //MemoryData mem = mainOpt.GetMemoryData(parts, Vision.MainOptic.eInsp.Stack, upDown);
 
                 MemoryData mem = mainOpt.GetMemoryData(InfoPod.ePod.EIP_Cover, Vision.MainOptic.eInsp.Stack, Vision.eUpDown.Front);
 
                 FocusStacking_new fs = new FocusStacking_new(mem);
                 int nCamWidth = ZStackGrabMode.m_camera.GetRoiSize().X;
                 int nCamHeight = ZStackGrabMode.m_camera.GetRoiSize().Y;
-                //nstep = mem.p_nCount-1;
+                nstep = mem.p_nCount - 1;
 
-                //double dstep = Math.Abs(nendPos - nstartPos)/nstep;
-                //for (int step=0;step<nstep;step++)
-                //{
-                //    double dPosZ = nstartPos + step * dstep;
-                //    //if (m_module.Run(m_module.Move(mainOpt.m_axisZ, dPosZ)))
-                //    //    return p_sInfo;
-                //    if (m_module.Run(axisZ.StartMove(dPosZ)))
-                //        return p_sInfo;
-                //    if (m_module.Run(axisZ.WaitReady()))
-                //        return p_sInfo;
 
-                //    IntPtr ptr = mem.GetPtr(step);
-                //    camZStack.LiveGrab();
+                double dstep = Math.Abs(nendPos - nstartPos) / nstep;
 
-                //    Parallel.For(0, nCamHeight, (i) => {
-                //        Marshal.Copy(camZStack.m_ImageLive.m_aBuf, i* nCamWidth, (IntPtr)((long)ptr + (i * mem.W)), nCamWidth);
-                //    });
+                for (int step = 0; step < nstep; step++)
+                {
+                    double dPosZ = nstartPos + step * dstep;
+                    //if (m_module.Run(m_module.Move(mainOpt.m_axisZ, dPosZ)))
+                    //    return p_sInfo;
+                    if (m_module.Run(axisZ.StartMove(dPosZ)))
+                        return p_sInfo;
+                    if (m_module.Run(axisZ.WaitReady()))
+                        return p_sInfo;
 
-                //    Thread.Sleep(100);
-                //}
+                    IntPtr ptr = mem.GetPtr(step);
+                    camZStack.LiveGrab();
+
+                    Parallel.For(0, nCamHeight, (i) =>
+                    {
+                        Marshal.Copy(camZStack.m_ImageLive.m_aBuf, i * nCamWidth, (IntPtr)((long)ptr + (i * mem.W)), nCamWidth);
+                    });
+
+                    Thread.Sleep(100);
+                }
 
                 fs.Run(nCamWidth, nCamHeight);
             }
