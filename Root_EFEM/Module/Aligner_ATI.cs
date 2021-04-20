4f5cc911c74f97951132cae2df9b494277cb3146
atijhan <jhan@ati2000.co.kr>
2021-04-20 10:10:00 +0900

    remove space

--- Root_EFEM/Module/Aligner_ATI.cs
+++ Root_EFEM/Module/Aligner_ATI.cs
@@ -71,9 +71,9 @@ namespace Root_EFEM.Module
             Axis m_axisX;
             Axis m_axisZ;
             Axis m_axisRotate;
-            DIO_IO m_dioVacuum; 
+            DIO_IO m_dioVacuum;
             DIO_O m_doBlow;
-            DIO_I2O2 m_dioGuide; 
+            DIO_I2O2 m_dioGuide;
             public void GetTools(ToolBox toolBox, Aligner_ATI module, bool bInit)
             {
                 module.p_sInfo = toolBox.GetDIO(ref m_dioVacuum, module, p_id + "Vacuum");
@@ -82,17 +82,17 @@ namespace Root_EFEM.Module
                 module.p_sInfo = toolBox.GetAxis(ref m_axisX, module, p_id + "AxisX");
                 module.p_sInfo = toolBox.GetAxis(ref m_axisZ, module, p_id + "AxisZ");
                 module.p_sInfo = toolBox.GetAxis(ref m_axisRotate, module, p_id + "AxisRotate");
-                if  (bInit)
+                if (bInit)
                 {
                     InitPosX();
                     InitPosZ();
-                    InitPosRotate(); 
+                    InitPosRotate();
                 }
             }
 
             #region Vacuum
             double m_secBlow = 0.5;
-            double m_secVac = 1; 
+            double m_secVac = 1;
             public string RunVacuum(bool bOn)
             {
                 m_dioVacuum.Write(bOn);
@@ -118,7 +118,7 @@ namespace Root_EFEM.Module
             public string RunGuide(bool bGuide)
             {
                 m_dioGuide.Write(bGuide);
-                return m_dioGuide.WaitDone(); 
+                return m_dioGuide.WaitDone();
             }
             #endregion
 
@@ -132,7 +132,7 @@ namespace Root_EFEM.Module
             }
             void InitPosX()
             {
-                m_axisX.AddPos(Enum.GetNames(typeof(ePosX))); 
+                m_axisX.AddPos(Enum.GetNames(typeof(ePosX)));
             }
 
             public string RunMoveX(ePosX ePosX, double fOffset)
@@ -193,7 +193,7 @@ namespace Root_EFEM.Module
             public string p_id { get; set; }
             public Flipper(string id)
             {
-                p_id = id; 
+                p_id = id;
             }
 
         }
@@ -407,17 +407,17 @@ namespace Root_EFEM.Module
 
         #region Axis Rotate
         int m_lRotate = 40000;
-        int m_nRotateBack = 0; 
+        int m_nRotateBack = 0;
         string Rotate(double fPulse)
         {
             double fNow = m_axisRotate.p_posCommand;
             while ((fNow - fPulse) > m_lRotate / 2) fNow -= m_lRotate;
             while ((fPulse - fNow) > m_lRotate / 2) fNow += m_lRotate;
-            SetRotatePosition(fNow); 
+            SetRotatePosition(fNow);
             if (fPulse < fNow)
             {
                 m_axisRotate.StartMove(fPulse - m_nRotateBack);
-                if (Run(m_axisRotate.WaitReady())) return p_sInfo; 
+                if (Run(m_axisRotate.WaitReady())) return p_sInfo;
             }
             m_axisRotate.StartMove(fPulse);
             if (Run(m_axisRotate.WaitReady())) return p_sInfo;
@@ -442,7 +442,7 @@ namespace Root_EFEM.Module
             m_lRotate = tree.Set(m_lRotate, m_lRotate, "PpR", "Pulse per Round (pulse)");
             m_nRotateBack = tree.Set(m_nRotateBack, m_nRotateBack, "Rotate Back", "Rotate Back Pulse (pulse)");
             m_secVac = tree.Set(m_secVac, m_secVac, "Vacuum", "Stage Vacuum On Timeout (sec)");
-            m_secBlow = tree.Set(m_secBlow, m_secBlow, "Blow", "Stage Vacuum Blow Time (sec)"); 
+            m_secBlow = tree.Set(m_secBlow, m_secBlow, "Blow", "Stage Vacuum Blow Time (sec)");
             RunTreeAlign(tree.GetTree("Grab"));
         }
         #endregion
@@ -484,7 +484,7 @@ namespace Root_EFEM.Module
         public string IsGetOK(int nID)
         {
             if (p_eState != eState.Ready) return p_id + " eState not Ready";
-            if (p_infoWafer == null) return p_id + " IsGetOK - InfoWafer not Exist"; 
+            if (p_infoWafer == null) return p_id + " IsGetOK - InfoWafer not Exist";
             return "OK";
         }
 
@@ -498,7 +498,7 @@ namespace Root_EFEM.Module
 
         public int GetTeachWTR(InfoWafer infoWafer = null)
         {
-            if (infoWafer == null) infoWafer = p_infoWafer; 
+            if (infoWafer == null) infoWafer = p_infoWafer;
             return m_waferSize.GetData(infoWafer.p_eSize).m_teachWTR;
         }
 
@@ -511,7 +511,7 @@ namespace Root_EFEM.Module
         public string BeforePut(int nID)
         {
             if (p_infoWafer != null) return p_id + " BeforePut : InfoWafer != null";
-            SetRotatePosition(0); 
+            SetRotatePosition(0);
             return CheckGetPut();
         }
 
@@ -536,7 +536,7 @@ namespace Root_EFEM.Module
             InfoWafer,
             Sensor
         }
-        eCheckWafer m_eCheckWafer = eCheckWafer.Sensor; 
+        eCheckWafer m_eCheckWafer = eCheckWafer.Sensor;
         public bool IsWaferExist(int nID)
         {
             switch (m_eCheckWafer)
@@ -562,7 +562,7 @@ namespace Root_EFEM.Module
 
         void RunTreeSetup(Tree tree)
         {
-            m_eCheckWafer = (eCheckWafer)tree.Set(m_eCheckWafer, m_eCheckWafer, "CheckWafer", "CheckWafer"); 
+            m_eCheckWafer = (eCheckWafer)tree.Set(m_eCheckWafer, m_eCheckWafer, "CheckWafer", "CheckWafer");
             m_szAlignROI = tree.Set(m_szAlignROI, m_szAlignROI, "Cameara AOI", "Camera AOI (pixel)");
             RunTreeWafer(tree.GetTree("Default Wafer", false));
             RunTreeRotate(tree.GetTree("Rotate", false));
@@ -621,12 +621,12 @@ namespace Root_EFEM.Module
         double m_secGrab = 5;
         public string AlignGrab(Aligner_ATI_AOI.Data data)
         {
-            m_aoi.m_data = data; 
+            m_aoi.m_data = data;
             m_aoiMax = null;
             StopWatch stopWatch = new StopWatch();
             double vGrabPulse = m_lRotate / m_secGrab;
             double pulseAcc = vGrabPulse * (m_secGrabAcc + 0.1) / 2;
-            SetRotatePosition(-pulseAcc); 
+            SetRotatePosition(-pulseAcc);
             m_doLightSide.Write(true);
             m_doLightCoaxial.Write(false);
             if (Run(AxisMoveAlign(ePosAlign.Align, true))) return p_sInfo;
@@ -680,15 +680,15 @@ namespace Root_EFEM.Module
         public string RunAlignExact(double degAccuracy)
         {
             if (m_aoiMax == null) return "Need Find Notch";
-            m_camAlign.SetMemoryData(m_memoryExact); 
+            m_camAlign.SetMemoryData(m_memoryExact);
             for (int n = 0; n < c_lExact; n++)
             {
                 double posAlign = m_aoiMax.GetNotchPos(m_lRotate);
                 string sRotate = Rotate(posAlign);
                 if (sRotate != "OK") return "Run Align Exact Rotate : " + sRotate;
                 m_aExactAOI[n].m_posGrab = m_axisRotate.p_posCommand;
-                m_camAlign.GrabOne(n); 
-                m_aoi.Inspect(m_aExactAOI[n]); 
+                m_camAlign.GrabOne(n);
+                m_aoi.Inspect(m_aExactAOI[n]);
                 m_aoiMax = m_aExactAOI[n];
             }
             return "Can't Find Notch"; //forget
@@ -719,7 +719,7 @@ namespace Root_EFEM.Module
             m_waferSize = new InfoWafer.WaferSize(id, false, false);
             m_aoi = new Aligner_ATI_AOI(m_log);
             base.InitBase(id, engineer);
-            InitInfoWaferUI(); 
+            InitInfoWaferUI();
         }
 
         public override void ThreadStop()
@@ -849,7 +849,7 @@ namespace Root_EFEM.Module
                 if (EQ.p_bSimulate) return "OK";
                 Aligner_ATI_AOI.AOI aoi = GetAOI();
                 if (aoi == null) return "AOI not Selected";
-                m_module.m_aoi.m_data = m_aoiData; 
+                m_module.m_aoi.m_data = m_aoiData;
                 aoi.m_maxNotch = null;
                 return m_module.m_aoi.Inspect(aoi);
             }
@@ -911,7 +911,7 @@ namespace Root_EFEM.Module
             public override string Run() //forget
             {
                 if (EQ.p_bSimulate) return "OK";
-                m_module.RunVacuum(true); 
+                m_module.RunVacuum(true);
                 m_module.p_bLightCoaxial = true;
 
                 if (m_module.Run(m_module.m_axisCamAlign.StartMove(ePosAlign.Align))) return p_sInfo;
