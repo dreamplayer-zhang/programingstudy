59d01cce11c2745ebfa5a5d3cc7ffc14290f4cf3
김민규 <forget018@naver.com>
2021-04-28 14:50:45 +0900

    Pine2 Project

---
+++ Root_Pine2/Engineer/Pine2_Handler_UI.xaml.cs
@@ -0,0 +1,39 @@
+﻿using RootTools.Module;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace Root_Pine2.Engineer
+{
+    /// <summary>
+    /// Pine2_Handler_UI.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Pine2_Handler_UI : UserControl
+    {
+        public Pine2_Handler_UI()
+        {
+            InitializeComponent();
+        }
+
+        Pine2_Handler m_handler;
+        public void Init(Pine2_Handler handler)
+        {
+            m_handler = handler;
+            DataContext = handler;
+            moduleListUI.Init(handler.p_moduleList);
+            gafUI.Init(handler.m_gaf);
+            InitTabControl();
+        }
+
+        void InitTabControl()
+        {
+            foreach (KeyValuePair<ModuleBase, UserControl> kv in m_handler.p_moduleList.m_aModule)
+            {
+                TabItem tabItem = new TabItem();
+                tabItem.Header = kv.Key.p_id;
+                tabItem.Content = kv.Value;
+                tabItem.Background = m_handler.p_brushModule;
+                tabModule.Items.Add(tabItem);
+            }
+        }
+    }
+}
---
+++ Root_Pine2/Module/MGZ_EV.cs
@@ -0,0 +1,36 @@
+﻿using RootTools;
+using RootTools.Module;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Root_Pine2.Module
+{
+    public class MGZ_EV : ModuleBase
+    {
+        #region ToolBox
+
+        public override void GetTools(bool bInit)
+        {
+            //p_sInfo = m_toolBox.GetDIO(ref m_dioEV, this, "Elevator", "Down", "Up");
+            if (bInit) InitTools();
+        }
+
+        void InitTools()
+        {
+        }
+        #endregion
+
+        public MGZ_EV(string id, IEngineer engineer)
+        {
+            base.InitBase(id, engineer);
+        }
+
+        public override void ThreadStop()
+        {
+            base.ThreadStop();
+        }
+    }
+}
