2caf9f7500da5b8e03afb147fafc770ce5c2f13f
조은성 <escho@ati2000.co.kr>
2021-05-03 15:57:02 +0900

    AOP01 포장부 RunStep Dialog 뷰모델 추가

---
+++ Root_AOP01_Packing/UI/3. RUN/Dlg_RunStepViewModel.cs
@@ -0,0 +1,40 @@
+﻿using RootTools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Root_AOP01_Packing
+{
+    public class Dlg_RunStepViewModel : ObservableObject, IDialogRequestClose
+    {
+        MainWindow m_MainWindow;
+        public AOP01_Engineer m_Engineer;
+
+        public event EventHandler<DialogCloseRequestedEventArgs> CloseRequested;
+
+        public Dlg_RunStepViewModel(MainWindow main, AOP01_Engineer engineer)
+        {
+            m_MainWindow = main;
+            m_Engineer = engineer;
+            Init();
+        }
+
+        private void Init()
+        {
+        }
+
+        public ICommand cmdClose
+        {
+            get
+            {
+                return new RelayCommand(() =>
+                {
+                    CloseRequested(this, new DialogCloseRequestedEventArgs(false));
+                });
+            }
+        }
+    }
+}
