b9b0c389ea7160b68068c3b067adaf7a9083afcc
김민규 <forget018@naver.com>
2021-06-17 13:32:44 +0900

    Cognex Barcode DM150

---
+++ RootTools/Camera/CognexDM150/Camera_CognexDM150_UI.xaml.cs
@@ -0,0 +1,32 @@
+﻿using RootTools.Trees;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RootTools.Camera.CognexDM150
+{
+    /// <summary>
+    /// Camera_CognexDM150_UI.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class Camera_CognexDM150_UI : UserControl
+    {
+        public Camera_CognexDM150_UI()
+        {
+            InitializeComponent();
+        }
+
+        Camera_CognexDM150 m_cam;
+        public void Init(Camera_CognexDM150 cam)
+        {
+            m_cam = cam;
+            DataContext = cam;
+            treeRootUI.Init(cam.p_treeRoot);
+            cam.RunTree(Tree.eMode.Init);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            m_cam.ReadBCD();
+        }
+
+    }
+}
