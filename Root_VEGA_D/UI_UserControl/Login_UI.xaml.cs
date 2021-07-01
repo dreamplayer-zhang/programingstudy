fbaaaa7df9bf28ac579416cec0c2c8439b2e7508
shyoo <shyoo@ati2000.co.kr>
2021-07-01 14:55:58 +0900

    VEGA-D PM UI 수정, Login 창 수정

--- Root_VEGA_D/UI_UserControl/Login_UI.xaml.cs
+++ Root_VEGA_D/UI_UserControl/Login_UI.xaml.cs
@@ -27,6 +27,8 @@ namespace Root_VEGA_D
 			m_engineer = engineer;
 			textBlockLoginLevel.DataContext = m_engineer.m_login;
 			textBlockLoginLevel.Foreground = m_engineer.m_login.p_sUserName == "Logout" ? Brushes.Red : Brushes.Green;
+
+			textBoxID.Focus();
 		}
 
 		private void Login_Click(object sender, RoutedEventArgs e)
