e67fb7a7f97fda69de321ac43064b9bee56ac114
김민규 <forget018@naver.com>
2021-07-05 15:30:24 +0900

    Pine2 SnapInfo

--- Root_Pine2/Module/Boats.cs
+++ Root_Pine2/Module/Boats.cs
@@ -199,7 +199,7 @@ namespace Root_Pine2.Module
                     m_aBoat[eWorks]._sRecipe = "";
                     m_aBoat[eWorks].p_sRecipe = sRecipe; 
                 }
-                m_vision.SendSnapInfo(eWorks);
+                m_vision.SendSnapInfo(m_aBoat[eWorks].GetSnapInfo());
                 m_aBoat[eWorks].p_eStep = Boat.eStep.Run;
                 m_aBoat[eWorks].m_doTriggerSwitch.Write(true);
                 int xLine = m_aBoat[eWorks].m_recipe.m_aSnap.Count;
