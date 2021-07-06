8c929674a2fb59396b929d8e71f6b5a3e5c81b57
atijhan <jhan@ati2000.co.kr>
2021-07-06 13:46:24 +0900

    add RAC align key base

---
+++ Root_WindII/UI/SETUP/CAMERA/MotionViewer/MotionViewer_ViewModel.cs
@@ -0,0 +1,66 @@
+﻿using RootTools.Control;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Root_WindII
+{
+    public class MotionViewer_ViewModel : ObservableObject
+    {
+
+        Axis axisX;
+        Axis axisY;
+        Axis axisTheta;
+        Axis axisZ;
+
+
+        public MotionViewer_ViewModel(Axis axisX, Axis axisY, Axis axisTheta, Axis axisZ)
+        {
+            this.axisX = axisX;
+            this.axisY = axisY;
+            this.axisTheta = axisTheta;
+            this.axisZ = axisZ;
+        }
+
+        #region [Properties]
+        public Axis AxisPositionX
+        {
+            get => this.axisX;
+            set
+            {
+                SetProperty(ref this.axisX, value);
+            }
+        }
+
+        public Axis AxisPositionY
+        {
+            get => this.axisY;
+            set
+            {
+                SetProperty(ref this.axisY, value);
+            }
+        }
+
+        public Axis AxisPositionTheta
+        {
+            get => this.axisTheta;
+            set
+            {
+                SetProperty(ref this.axisTheta, value);
+            }
+        }
+
+        public Axis AxisPositionZ
+        {
+            get => this.axisZ;
+            set
+            {
+                SetProperty(ref this.axisZ, value);
+            }
+        }
+
+        #endregion
+    }
+}
