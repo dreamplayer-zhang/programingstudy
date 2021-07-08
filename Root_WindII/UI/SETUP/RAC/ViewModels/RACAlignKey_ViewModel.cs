3eac5d1dc9daad1a2bc804b66d5aab53726b317c
atijhan <jhan@ati2000.co.kr>
2021-07-08 13:55:39 +0900

    remove align information ui

--- Root_WindII/UI/SETUP/RAC/ViewModels/RACAlignKey_ViewModel.cs
+++ Root_WindII/UI/SETUP/RAC/ViewModels/RACAlignKey_ViewModel.cs
@@ -77,46 +77,6 @@ namespace Root_WindII
                 SetProperty<Image>(ref this.selectedFeatureItem, value);
             }
         }
-
-        private long firstAxisPositionX = 0;
-        public long FirstAxisPositionX
-        {
-            get => this.firstAxisPositionX;
-            set
-            {
-                SetProperty(ref this.firstAxisPositionX, value);
-            }
-        }
-
-        private long firstAxisPositionY = 0;
-        public long FirstAxisPositionY
-        {
-            get => this.firstAxisPositionY;
-            set
-            {
-                SetProperty(ref this.firstAxisPositionY, value);
-            }
-        }
-
-        private long secondAxisPositionX = 0;
-        public long SecondAxisPositionX
-        {
-            get => this.secondAxisPositionX;
-            set
-            {
-                SetProperty(ref this.secondAxisPositionX, value);
-            }
-        }
-
-        private long secondAxisPositionY = 0;
-        public long SecondAxisPositionY
-        {
-            get => this.secondAxisPositionY;
-            set
-            {
-                SetProperty(ref this.secondAxisPositionY, value);
-            }
-        }
         #endregion
 
 
@@ -242,35 +202,6 @@ namespace Root_WindII
             });
         }
 
-        public ICommand btnSetFirstAxisPositionCommond
-        {
-            get => new RelayCommand(() =>
-            {
-                this.FirstAxisPositionX = (long)VisionModule.AxisXY.p_axisX.p_posActual;
-                this.FirstAxisPositionY = (long)VisionModule.AxisXY.p_axisY.p_posActual;
-
-                FrontAlignRecipe alignRecipe = GlobalObjects.Instance.Get<RecipeAlign>().GetItem<FrontAlignRecipe>();
-
-                alignRecipe.FirstSearchPointX = this.FirstAxisPositionX;
-                alignRecipe.FirstSearchPointY = this.FirstAxisPositionY;
-
-            });
-        }
-
-        public ICommand btnSetSecondAxisPositionCommond
-        {
-            get => new RelayCommand(() =>
-            {
-                this.SecondAxisPositionX = (long)VisionModule.AxisXY.p_axisX.p_posActual;
-                this.SecondAxisPositionY = (long)VisionModule.AxisXY.p_axisY.p_posActual;
-
-                FrontAlignRecipe alignRecipe = GlobalObjects.Instance.Get<RecipeAlign>().GetItem<FrontAlignRecipe>();
-
-                alignRecipe.SecondSearchPointX = this.SecondAxisPositionX;
-                alignRecipe.SecondSearchPointY = this.SecondAxisPositionY;
-            });
-        }
-
         public ICommand btnFeatureAddCommand
         {
             get => new RelayCommand(() =>
@@ -347,11 +278,6 @@ namespace Root_WindII
         public void LoadRecipe()
         {
             FrontAlignRecipe alignRecipe = GlobalObjects.Instance.Get<RecipeAlign>().GetItem<FrontAlignRecipe>();
-
-            this.FirstAxisPositionX = alignRecipe.FirstSearchPointX;
-            this.FirstAxisPositionY = alignRecipe.FirstSearchPointY;
-            this.SecondAxisPositionX = alignRecipe.SecondSearchPointX;
-            this.SecondAxisPositionY = alignRecipe.SecondSearchPointY;
 
             RefreshFeatureItemList();
         }
