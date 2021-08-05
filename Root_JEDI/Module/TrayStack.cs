ad69d690f2457f2206fcbba0060031ce14418520
김민규 <forget018@naver.com>
2021-08-05 15:19:43 +0900

    JEDI ...

--- Root_JEDI/Module/TrayStack.cs
+++ Root_JEDI/Module/TrayStack.cs
@@ -1,13 +1,23 @@
-﻿using RootTools.Module;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RootTools.Control;
+using RootTools.Module;
 
 namespace Root_JEDI.Module
 {
     public class TrayStack : ModuleBase
     {
+        #region ToolBox
+        public DIO_I4O m_dioAlignY;
+        public DIO_I2O m_dioAlignX;
+        public DIO_Is[] m_diCheck = new DIO_Is[4] { null, null, null, null };
+        public override void GetTools(bool bInit)
+        {
+            m_toolBox.GetDIO(ref m_dioAlignX, this, "AlignX", "Off", "Align");
+            m_toolBox.GetDIO(ref m_dioAlignY, this, "AlignY", "Off", "Align");
+            m_toolBox.GetDIO(ref m_diCheck[0], this, "Check 1F", new string[2] { "A", "B" });
+            m_toolBox.GetDIO(ref m_diCheck[1], this, "Check 2F", new string[2] { "A", "B" });
+            m_toolBox.GetDIO(ref m_diCheck[2], this, "Check 3F", new string[2] { "A", "B" });
+            m_toolBox.GetDIO(ref m_diCheck[3], this, "Check 4F", new string[2] { "A", "B" });
+        }
+        #endregion
     }
 }
