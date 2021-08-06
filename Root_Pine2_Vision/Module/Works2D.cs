9e74a1405f863764e0bd9a7f1a7bf0ead65a4342
Athur <gsyun@ati2000.co.kr>
2021-08-06 20:06:27 +0900

    Pine2. ReqSnap수정.

--- Root_Pine2_Vision/Module/Works2D.cs
+++ Root_Pine2_Vision/Module/Works2D.cs
@@ -176,6 +176,7 @@ namespace Root_Pine2_Vision.Module
                 if (asSend[1] == eProtocol.Snap.ToString())
                 {
                     string sRecipe = asSend[2].Substring(0, asSend[2].Length - 5);
+                    m_vision.m_RunningRecipe[p_eWorks].RecipeOpen(m_sRecipe);
                     string sInfo = m_vision.ReqSnap(sRecipe, p_eWorks);
                     m_tcpip.Send(sSend.Substring(0, sSend.Length - 1) + "," + sInfo + "]"); 
                 }
